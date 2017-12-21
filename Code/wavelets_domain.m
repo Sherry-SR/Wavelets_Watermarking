63cc457a7dff5cd2202b82b94b27b1d76aaec977
Sherry <shenrui.seu@foxmail.com>
2017-12-21 16:19:17 -0500

    add 6 attacks, add quantization

--- Code/wavelets_domain.m
+++ Code/wavelets_domain.m
@@ -7,9 +7,8 @@ InfoDataName = '../Data/InfoS1/000075.txt';
 fileID = fopen(InfoDataName,'r');
 WM = textscan(fileID,'%s %s','Delimiter','|');
 fclose(fileID);
-IMAGE_RAW = double(dicomread(RawFilename));
+IMAGE = double(dicomread(RawFilename));
 HEADER = dicominfo(RawFilename);
-IMAGE = rescale(IMAGE_RAW);
 
 %% ====================== ROI segmentation ======================
 %[B, MaskImg] = segmentation(IMAGE); %example of using segmentation
@@ -49,15 +48,16 @@ end
 bin_len = 8;
 code_len = 31;
 pos_shift = 31;
-d{1} = double(watermark_encode(int64(2*255*d{1}),Diagnosis,bin_len,pos_shift,code_len))/2/255;
-d{2} = double(watermark_encode(int64(2*255*d{2}),PhysicianInfo,bin_len,pos_shift,code_len))/2/255;
-v{1} = double(watermark_encode(int64(2*255*v{1}),PatientInfo,bin_len,pos_shift,code_len))/2/255;
-h{1} = double(watermark_encode(int64(2*255*h{1}),ImageInfo,bin_len,pos_shift,code_len))/2/255;
+Q = 32;
+d{1} = double(watermark_encode(int64(2*Q*d{1}),Diagnosis,bin_len,pos_shift,code_len))/2/Q;
+d{2} = double(watermark_encode(int64(2*Q*d{2}),PhysicianInfo,bin_len,pos_shift,code_len))/2/Q;
+v{1} = double(watermark_encode(int64(2*Q*v{1}),PatientInfo,bin_len,pos_shift,code_len))/2/Q;
+h{1} = double(watermark_encode(int64(2*Q*h{1}),ImageInfo,bin_len,pos_shift,code_len))/2/Q;
 
 Im_re = ihaart2(a,h,v,d);
 figure;
-subplot(1,2,1);imshow(IMAGE);title('original image');
-subplot(1,2,2);imshow(Im_re);title('watermarked image');
+subplot(1,2,1);imshow(IMAGE,[]);title('original image');
+subplot(1,2,2);imshow(Im_re,[]);title('watermarked image');
 
 %% ====================== evaluation ======================
 
@@ -71,44 +71,36 @@ subplot(1,2,1);imshow(Im,[]);title('original image in wavelets domain');
 subplot(1,2,2);imshow(Im_co,[]);title('watermarked image in wavelets domain');
 
 [m,n] = size(IMAGE);
-Im_re_scale = rescale(Im_re);
-IMAGE = rescale(IMAGE);
 figure;
 subplot(1,2,1);imshow(Im_co-Im,[]);title('differential image in wavelets domain');
-subplot(1,2,2);imshow(Im_re_scale-IMAGE,[]);title('differential image in spatial domain');
-MSE = sum(sum((Im_re_scale-IMAGE).*(Im_re_scale-IMAGE)))/(m*n);
-SNR = snr(IMAGE,IMAGE-Im_re_scale);
-PSNR = psnr(Im_re_scale,IMAGE);
+subplot(1,2,2);imshow(Im_re-IMAGE,[]);title('differential image in spatial domain');
+MSE = sum(sum((Im_re-IMAGE).*(Im_re-IMAGE)))/(m*n);
+SNR = snr(IMAGE,IMAGE-Im_re);
+PSNR = psnr(Im_re,IMAGE,max(max(IMAGE)));
 
 %% ====================== watermark decode ======================
-bin_len2 = 8;
-code_len2 = 31;
-pos_shift2 = 31;
+
 [a2,h2,v2,d2] = haart2(Im_re,Level);
-Diagnosis_re = watermark_decode(int64(2*255*d2{1}),bin_len2,pos_shift2,code_len2);
-PhysicianInfo_re = watermark_decode(int64(2*255*d2{2}),bin_len2,pos_shift2,code_len2);
-PatientInfo_re = watermark_decode(int64(2*255*v2{1}),bin_len2,pos_shift2,code_len2);
-ImageInfo_re = watermark_decode(int64(2*255*h2{1}),bin_len2,pos_shift2,code_len2);
+Diagnosis_re = watermark_decode(int64(2*Q*d2{1}),bin_len,pos_shift,code_len);
+PhysicianInfo_re = watermark_decode(int64(2*Q*d2{2}),bin_len,pos_shift,code_len);
+PatientInfo_re = watermark_decode(int64(2*Q*v2{1}),bin_len,pos_shift,code_len);
+ImageInfo_re = watermark_decode(int64(2*Q*h2{1}),bin_len,pos_shift,code_len);
 
 %% ====================== attack testing ======================
-attacktype = {'gaussian'}; %example of using attack testing
-parameter = {[0 0.01]};
+attacktype = {'flip','gaussfilt','medfilt','modification','salt & pepper','gaussian'}; %example of using attack testing
+parameter = {2, 0.2, 5, [32 0], 0.01, [0 0.01]};
 AttackedImg = attack(Im_re,attacktype,parameter);
-Im_re_scale = rescale(Im_re);
-AttackedImg_scale = rescale(AttackedImg{1});
 figure;
-subplot(1,2,1);imshow(Im_re_scale);title('watermarked image');
-subplot(1,2,2);imshow(AttackedImg_scale);title('watermarked image after attacked');
-MSE2 = sum(sum((AttackedImg_scale-IMAGE).*(AttackedImg_scale-IMAGE)))/(m*n);
-SNR2 = snr(IMAGE,IMAGE-AttackedImg_scale);
-PSNR2 = psnr(AttackedImg_scale,IMAGE);
+subplot(1,2,1);imshow(Im_re,[]);title('watermarked image');
+subplot(1,2,2);imshow(AttackedImg{1},[]);title('watermarked image after attacking');
+MSE2 = sum(sum((AttackedImg{1}-IMAGE).*(AttackedImg{1}-IMAGE)))/(m*n);
+SNR2 = snr(IMAGE,IMAGE-AttackedImg{1});
+PSNR2 = psnr(AttackedImg{1},IMAGE,max(max(IMAGE)));
 
 %% ====================== watermark retrieve ======================
-bin_len3 = 8;
-code_len3 = 31;
-pos_shift3 = 31;
+
 [a3,h3,v3,d3] = haart2(AttackedImg{1},Level);
-Diagnosis_re2 = watermark_decode(int64(2*255*d3{1}),bin_len3,pos_shift3,code_len3);
-PhysicianInfo_re2 = watermark_decode(int64(2*255*d3{2}),bin_len3,pos_shift3,code_len3);
-PatientInfo_re2 = watermark_decode(int64(2*255*v3{1}),bin_len3,pos_shift3,code_len3);
-ImageInfo_re2 = watermark_decode(int64(2*255*h3{1}),bin_len3,pos_shift3,code_len3);
+Diagnosis_re2 = watermark_decode(int64(2*Q*d3{1}),bin_len,pos_shift,code_len);
+PhysicianInfo_re2 = watermark_decode(int64(2*Q*d3{2}),bin_len,pos_shift,code_len);
+PatientInfo_re2 = watermark_decode(int64(2*Q*v3{1}),bin_len,pos_shift,code_len);
+ImageInfo_re2 = watermark_decode(int64(2*Q*h3{1}),bin_len,pos_shift,code_len);
